3e336ae5e3dd6a3393d021452493f909f9e6f922
Rick Warrington <phial1020@gmail.com>
2011-05-31 13:14:18 -0600

    Cleaning up some whitespace.
    Rick

--- main.cpp
+++ main.cpp
@@ -47,28 +47,23 @@ void DisplayProgress(int current, int max) {
 string GetFilename(char *path) {
 	string temp = path;
 	size_t found;
-
 	found = temp.find_last_of("\\");
-
 	return temp.substr(found + 1, temp.size() - found);
 }
 
 string SetWorkingDirectory(char *path) {
 	string temp = path;
 	size_t found;
-
 	found = temp.find_last_of("\\");
-
 	return temp.substr(0, found);
 }
 
 string CreateDirectoryNameFromFile(char * filename) {
 	string name = filename;
 	string foldername;
-
 	size_t found;
+
 	found = name.find_last_of("\\");
 	foldername = name.substr(found + 1, name.size() - found - 5);
-
 	return foldername;
 }
